79c21dab09db7033173ee228e41b2b3c241faf59
ckaraman <cagrkaraman@gmail.com>
2020-10-02 22:11:10 +0300

    dependency update

--- init.R
+++ init.R
@@ -1,3 +1,3 @@
-Packages <- c("RSQLite","stringr","SPEI","ggplot2","zoo","Kendall","DT","trend","ggpubr","leaflet","plotly")
+# Packages <- c("RSQLite","stringr","SPEI","ggplot2","zoo","Kendall","DT","trend","ggpubr","leaflet","plotly")
 
-install.packages(Packages)
+install.packages("plotly")
